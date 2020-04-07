032d56d12ce4aa33fa003e1a89336d8c331098f6
Asim Hyder <ahyder2@hawk.iit.edu>
2020-04-06 22:38:09 -0500

    Made changes to collapse script to try to fix it

--- javascript/collapse.js
+++ javascript/collapse.js
@@ -1,10 +1,11 @@
 function collapse() {
 		var sec = document.getElementsByClassName("section");
 		var information = document.getElementsByClassName("information");
-	 	if (information.style.display === "none") {
+		var disp = information.style.display.value;
+	 	if (disp === "none") {
 	 		information.style.display = "block";
 	 	}
-	 	if (information.style.display === "block"){
+	 	if (disp === "block"){
 	 		information.style.display = "none";
 	 	}
 }
