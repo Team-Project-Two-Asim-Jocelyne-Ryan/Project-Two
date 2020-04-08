b841c275972e6a058c6f1e22296c90c3db86f0e5
Asim Hyder <ahyder2@hawk.iit.edu>
2020-04-07 19:51:38 -0500

    Added CSS to provide feedbacl that a section is being hovered on and is active

--- javascript/collapse.js
+++ javascript/collapse.js
@@ -3,6 +3,7 @@ var i;
 function collapse() {
 	for (i = 0; i < sec.length; i++) {
 	 	sec[i].addEventListener("click", function(){
+	 		this.classList.toggle("activesec");
 	 		var info = this.nextElementSibling;
 	 		if (info.style.display === "block"){
 	 			info.style.display = "none";
