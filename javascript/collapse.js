4ee2b76743f99a70cc0c690f4ed5beb05b333c17
Asim Hyder <ahyder2@hawk.iit.edu>
2020-04-07 19:37:14 -0500

    Moved script to onload so only one click is required

--- javascript/collapse.js
+++ javascript/collapse.js
@@ -3,7 +3,6 @@ var i;
 function collapse() {
 	for (i = 0; i < sec.length; i++) {
 	 	sec[i].addEventListener("click", function(){
-	 		this.classList.toggle("active");
 	 		var info = this.nextElementSibling;
 	 		if (info.style.display === "block"){
 	 			info.style.display = "none";
@@ -11,6 +10,6 @@ function collapse() {
 	 		else {
 	 			info.style.display = "block";
 	 		}
-	 	})
+	 	});
 	 }
 }
