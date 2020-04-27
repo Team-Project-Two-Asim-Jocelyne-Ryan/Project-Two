a641eb33badee4d2f8941b5a4f5e830afc5e1fce
Asim Hyder <ahyder2@hawk.iit.edu>
2020-04-27 00:30:34 -0500

    Created a script for form validation when signing up

--- javascript/siginbutton.js
+++ javascript/siginbutton.js
@@ -1,5 +1,25 @@
-function Signup() {
-    alert(" Thanks for joining");
-   
-    return false;
-  }
+function Signup(){
+	var fn = document.forms["signup"]["fname"].value;
+	var ln = document.forms["signup"]["lname"].value;
+	var e = document.forms["signup"]["email"].value;
+	var p = document.forms["signup"]["pwd"].value;
+	if (fn.length < 1){
+		alert("Fill out first name");
+		return false;
+	}
+	else if (ln.length < 1){
+		alert("Fill out last name");
+		return false;
+	}
+	else if (e.length < 1){
+		alert("Fill out email");
+		return false;
+	}
+	else if (p.length < 15){
+		return false;
+	}
+	else{
+		alert("Thanks for joining!");
+		return true;
+	}
+}
